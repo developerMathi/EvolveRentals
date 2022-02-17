4f37ac05e5f8dfaeba176061de13d7baab28bf39
developerMathi <54667723+developerMathi@users.noreply.github.com>
2022-02-17 08:20:07 +0530

    ios changes

--- EvolveRentals/EvolveRentals.iOS/AppDelegate.cs
+++ EvolveRentals/EvolveRentals.iOS/AppDelegate.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using ContextMenu.iOS;
+using DLToolkit.Forms.Controls;
+using EvolveRentals.AuthHelpers;
 using Foundation;
+using PanCardView.iOS;
+using Plugin.FacebookClient;
 using UIKit;
 
 namespace EvolveRentals.iOS
@@ -22,10 +26,40 @@ namespace EvolveRentals.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            Rg.Plugins.Popup.Popup.Init();
+            FlowListView.Init();
             global::Xamarin.Forms.Forms.Init();
+            ContextMenuViewRenderer.Preserve();
+            Plugin.InputKit.Platforms.iOS.Config.Init();
+            string pagename = null;
+            string data = null;
+            CardsViewRenderer.Preserve();
             LoadApplication(new App());
-
+            global::Xamarin.Auth.Presenters.XamarinIOS.AuthenticationConfiguration.Init();
+            FacebookClientManager.OnActivated();
+            FacebookClientManager.Initialize(app, options);
             return base.FinishedLaunching(app, options);
+        }
+
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            // Convert NSUrl to Uri
+            var uri = new Uri(url.AbsoluteString);
+
+            // Load redirectUrl page
+            AuthenticationState.Authenticator.OnPageLoading(uri);
+
+            return true;
+        }
+
+        //public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        //{
+        //    return FacebookClientManager.OpenUrl(app, url, options);
+        //}
+
+        public override bool OpenUrl(UIApplication application, NSUrl url, string sourceApplication, NSObject annotation)
+        {
+            return FacebookClientManager.OpenUrl(application, url, sourceApplication, annotation);
         }
     }
 }
